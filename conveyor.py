b2523129d4c8bb8c5fe6c3187c14b90ef4e4b59e
arthur <wr80340@smail.nchu.edu.tw>
2025-02-28 11:57:21 +0800

    #remain shift error

--- conveyor.py
+++ conveyor.py
@@ -12,25 +12,34 @@ class Conveyor(sim.Component):
         conveyor_max_speed: int = 5,
         conveyor_min_speed: int = 0,
         conveyor_length: float = 20.0,
+        scan_interval: float = 1.0,
         *args,
         **kwargs,
     ):
         super().__init__(*args, **kwargs)
         self.from_buffer = from_buffer
         self.to_buffer = to_buffer
-        self.conveyor_speed = conveyor_speed
-        self.conveyor_max_speed = conveyor_max_speed
-        self.conveyor_min_speed = conveyor_min_speed
+        self.conveyor_speed = conveyor_speed * scan_interval
+        self.conveyor_max_speed = conveyor_max_speed * scan_interval
+        self.conveyor_min_speed = conveyor_min_speed * scan_interval
         self.conveyor_length = conveyor_length
-        self.reset_remain_length(offset=0)
+        self.scan_interval = scan_interval
+
+        self.reset_offset
+        self.reset_remain_length
         self.reset_remain_time
 
-    def reset_remain_length(self, offset: float):
-        self.remain_length = self.conveyor_length + offset
+    @property
+    def reset_offset(self):
+        self.offset = 0
+
+    @property
+    def reset_remain_length(self):
+        self.remain_length = self.conveyor_length - self.offset
 
     @property
     def reset_remain_time(self):
-        self.remain_time = 1
+        self.remain_time = self.scan_interval
 
     def process(self):
         while True:
@@ -44,19 +53,23 @@ class Conveyor(sim.Component):
                 product = self.from_store(self.from_buffer)
 
             production_complete = self.remain_length <= 0
+            self.reset_offset
             while not production_complete:
                 if self.remain_length - self.conveyor_speed >= 0:
-                    self.hold(min(1, self.remain_time))
+                    self.hold(self.remain_time)
                     self.remain_length -= self.conveyor_speed
-                    offset = 0
                     self.reset_remain_time
                 else:
-                    self.hold(self.remain_length / self.conveyor_speed)
-                    offset = self.remain_length - self.conveyor_speed
-                    self.remain_time = 1 - self.remain_length / self.conveyor_speed
+                    self.hold(
+                        self.scan_interval * (self.remain_length / self.conveyor_speed)
+                    )
+                    self.offset = -1 * (self.remain_length - self.conveyor_speed)
+                    self.remain_time = self.scan_interval * (
+                        1 - (self.remain_length / self.conveyor_speed)
+                    )
                     production_complete = True
 
-            self.reset_remain_length(offset=offset)
+            self.reset_remain_length
             while self.to_buffer.available_quantity() <= 0:
                 self.standby()
             self.to_store(self.to_buffer, product)
--- env.py
+++ env.py
@@ -4,13 +4,14 @@ from collections import defaultdict
 import numpy as np
 import salabim as sim
 
-from conveyor import Conveyor, FirstConveyor
+from conveyor import Conveyor
 from machine import Machine
 
-run_till = 100  # 1 hour
+run_till = 50  # 1 hour
 seed = 1122
-conveyor_1_speed = 3
-conveyor_2_speed = 3
+conveyor_1_speed = 4
+conveyor_2_speed = 4
+scan_interval = 0.1
 
 env = sim.Environment(
     # trace=True if run_till <= 100 else False,
@@ -24,10 +25,13 @@ head_buffer = sim.Store(name="前方緩存區", capacity=1)
 tail_buffer = sim.Store(name="後方緩存區", capacity=1)
 sn_sink = sim.Store(name="產品接收器", capacity=10000)
 
-conveyor1 = FirstConveyor(
+conveyor1 = Conveyor(
     name="前方傳輸帶",
+    from_buffer=None,
     to_buffer=head_buffer,
     conveyor_speed=conveyor_1_speed,
+    scan_interval=scan_interval,
+    env=env,
 )
 
 machine = Machine(
@@ -35,6 +39,7 @@ machine = Machine(
     from_buffer=head_buffer,
     to_buffer=tail_buffer,
     machine_cycle_time=2.83,
+    env=env,
 )
 
 conveyor2 = Conveyor(
@@ -42,6 +47,8 @@ conveyor2 = Conveyor(
     from_buffer=tail_buffer,
     to_buffer=sn_sink,
     conveyor_speed=conveyor_2_speed,
+    scan_interval=scan_interval,
+    env=env,
 )
 
 l = {}
@@ -67,10 +74,14 @@ class EnvScanner(sim.Component):
         while True:
 
             # wait until next scan
-            l[env.now()] = {
+            l[round(env.now(), 2)] = {
                 "c1": round(conveyor1.remain_length, 2),
-                "m": round(machine.scheduled_time(), 2),
+                "c1_s": round(conveyor1.conveyor_speed, 2),
+                "m": round(machine.scheduled_time() -env.now(), 3),
                 "c2": round(conveyor2.remain_length, 2),
+                # "of": round(conveyor1.offset, 2),
+                # "c1_rt": round(conveyor1.remain_time, 2),
+                # "c1_rl": round(conveyor1.remain_length, 2),
                 # "c2_status": conveyor2.status(),
             }
             # conveyor1.speed_accelerate(accelerate=0.3)
@@ -78,7 +89,7 @@ class EnvScanner(sim.Component):
             self.hold(self.scan_interval)
 
 
-env_scanner = EnvScanner(name="監視器", scan_interval=1)
+env_scanner = EnvScanner(name="監視器", scan_interval=scan_interval)
 env.run(run_till)
 
 # %%
