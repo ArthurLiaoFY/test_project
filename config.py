bcdf4085b3e57b18442f381125ca1d34be2c54ef
arthur <wr80340@smail.nchu.edu.tw>
2025-03-01 13:21:24 +0800

    #change config tyo py file

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
-{
-    "action_mapping_dict": {
-        "0": 0,
-        "1": -1,
-        "2": 1
-    },
+q_learn_kwargs = {
+    "action_mapping_dict": {0: 0, 1: -1, 2: 1},
     "learning_rate": 0.1,
     "explore_rate": 0.5,
     "learning_rate_min": 0.03,
@@ -11,5 +7,5 @@
     "learning_rate_decay": 0.999,
     "explore_rate_decay": 0.999,
     "discount_factor": 0.99,
-    "fully_explore_step": 0
+    "fully_explore_step": 0,
 }
